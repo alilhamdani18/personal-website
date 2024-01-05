840476cd35ea07dc32ca79b705c3e06476c617eb
alilhamdani18 <hamdanialil18@gmail.com>
2024-01-05 21:46:18 +0800

    add new feature dark-mode

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const switchToggle = document.getElementById('switchToggle');
-const check = document.getElementById('check')
-const hero = document.querySelector('.hero')
+const check = document.querySelector('.checkbox');
+const navbar = document.getElementById('navbar')
+const footer = document.querySelector('footer');
+const descHero = document.querySelectorAll('.desc h6');
+const cardText = document.querySelectorAll('.card-text');
 
-check.addEventListener('click', function() {
-  document.body.classList.toggle('dark')
-})
-
-switchToggle.addEventListener('click', function() {
-  hero.classList.toggle('dark');
-})
+check.addEventListener('click', function () {
+  document.body.classList.toggle('dark');
+  navbar.classList.toggle('dark')
+  descHero.forEach(element => {
+    element.classList.toggle('dark-text')
+  });
+  cardText.forEach(element => {
+    element.classList.toggle('dark-text')
+  });
+  footer.classList.toggle('dark-secondary');
+});
